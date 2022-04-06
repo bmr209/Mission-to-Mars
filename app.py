5aa71ad69714d190558834c34210606e9ac6da24
Brian Rincon <bmr209@Brians-MacBook-Air.local>
2022-04-05 17:29:05 -0700

    Adding web scraping files.

---
+++ app.py
@@ -0,0 +1,28 @@
+# Use Flask to render a template, redirect to another URL and create a URL
+from flask import Flask, render_template, redirect, url_for
+# Use PyMongo to interact with our Mongo database
+from flask_pymongo import PyMongo
+# To use the scraping code, convert from Jupyter notebook to Python
+import scraping
+
+# Set up Flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upset=True)
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run()
